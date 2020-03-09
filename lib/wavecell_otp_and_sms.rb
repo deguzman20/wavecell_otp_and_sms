9eeaa7b43236b038465580a1e3765faa10a88349
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-03-09 21:57:17 +0800

    Update config

--- lib/wavecell_otp_and_sms.rb
+++ lib/wavecell_otp_and_sms.rb
@@ -1,6 +1,8 @@
 require "wavecell_otp_and_sms/version"
 require "wavecell_otp_and_sms/configuration"
 require "wavecell_otp_and_sms/send_sms"
+require "wavecell_otp_and_sms/verify_otp"
+require "wavecell_otp_and_sms/send_otp"
 
 module WavecellOtpAndSms
   class Error < StandardError; end
