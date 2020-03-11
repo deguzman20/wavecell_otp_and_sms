6cfc96aab6485c73efb2dcce3e11cfd942f68881
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-03-11 09:36:46 +0800

    update

--- lib/wavecell_otp_and_sms/send_sms.rb
+++ lib/wavecell_otp_and_sms/send_sms.rb
@@ -35,6 +35,8 @@ module WavecellOtpAndSms
 
         uri = URI.parse("https://api.wavecell.com/sms/v1/#{sub_account}/single")
         request = Net::HTTP::Post.new(uri)
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36'
+        request.initialize_http_header({"User-Agent" => user_agent})
         request.content_type = "application/json"
         request["Authorization"] = "Bearer #{api_key}"
         request.body = "${ \"source\": \"#{source}\", \"destination\": \"#{destination}\", \"text\": \"#{text}\", \"encoding\": \"#{encoding}\" }"
