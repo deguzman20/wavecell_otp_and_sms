dc5b8de93853863cd8354bf3c47102bc8711ac3c
deguzman20 <alejandrogacumadeguzman@gmail.com>
2020-03-13 10:48:42 +0800

    Fixed verified OTP method

--- lib/wavecell_otp_and_sms/verify_otp.rb
+++ lib/wavecell_otp_and_sms/verify_otp.rb
@@ -22,7 +22,7 @@ module WavecellOtpAndSms
         api_key = WavecellOtpAndSms.configuration.api_key
         sub_account = WavecellOtpAndSms.configuration.sub_account
         details = [uid, code]
-        url = "https://api.wavecell.com/otp/v1/#{sub_account}/#{uid}?code=#{code}" + "?#{query_string}"
+        url = "https://api.wavecell.com/otp/v1/#{sub_account}/#{uid}?code=#{code}"
         HTTParty.post(url.to_str,
         :body => parameters.to_json,
         :headers => {
